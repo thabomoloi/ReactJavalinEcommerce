9c1d7e6493656cb37eb65e4650b05aa7a52b4b2f
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-10-26 03:40:55 +0200

    Add app config

--- src/main/java/com/oasisnourish/App.java
+++ src/main/java/com/oasisnourish/App.java
@@ -1,5 +1,7 @@
 package com.oasisnourish;
 
+import io.javalin.Javalin;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        var app = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(7070);
     }
 }
